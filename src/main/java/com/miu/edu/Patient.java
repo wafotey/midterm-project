4affc14e35dda5be65179601111d1ac106246e91
wafotey <wafotey@outlook.com>
2025-03-06 11:42:05 -0600

    modified ci/cd

--- src/main/java/com/miu/edu/Patient.java
+++ src/main/java/com/miu/edu/Patient.java
@@ -1,4 +1,46 @@
 package com.miu.edu;
 
 public class Patient {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Patient(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
